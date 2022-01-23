0532602791a9e1e4ecb2eca0f197c55904dad3e4
AlexanderBrovko <abrovko@optisystems24.ru>
2022-01-23 13:16:31 +0300

    refactored structure

--- src/com/brovko/leetcode/medium/AddTwoNumbers.java
+++ src/com/brovko/leetcode/medium/AddTwoNumbers.java
@@ -1,8 +1,7 @@
 package com.brovko.leetcode.medium;
 
+import com.brovko.leetcode.common.ListNode;
 import org.junit.Test;
-
-import java.util.Objects;
 
 import static org.junit.Assert.assertEquals;
 
@@ -93,22 +92,3 @@ public class AddTwoNumbers {
     }
 }
 
-class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ListNode listNode = (ListNode) o;
-        return val == listNode.val && Objects.equals(next, listNode.next);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(val, next);
-    }
-}
