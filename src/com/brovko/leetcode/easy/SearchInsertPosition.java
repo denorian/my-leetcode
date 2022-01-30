c0de4756bcce17be76db603e834b02d79dc24353
AlexanderBrovko <abrovko@optisystems24.ru>
2022-01-30 23:07:22 +0300

    SearchInsertPosition

---
+++ src/com/brovko/leetcode/easy/SearchInsertPosition.java
@@ -0,0 +1,68 @@
+package com.brovko.leetcode.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SearchInsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end =  nums.length - 1;
+
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if(nums[end] < target){
+            return end + 1;
+        } else if (nums[start] >= target){
+            return start;
+        } else {
+            return end;
+        }
+    }
+
+
+
+    @Test
+    public void test_1() {
+        assertEquals(2, searchInsert(new int[]{1,3,5,6}, 5));
+    }
+
+    @Test
+    public void test_2() {
+        assertEquals(1, searchInsert(new int[]{1,3,5,6}, 2));
+    }
+
+    @Test
+    public void test_3() {
+        assertEquals(4, searchInsert(new int[]{1,3,5,6}, 7));
+    }
+
+    @Test
+    public void test_4() {
+        assertEquals(0, searchInsert(new int[]{1,3,5,6}, 0));
+    }
+
+    @Test
+    public void test_5() {
+        assertEquals(0, searchInsert(new int[]{1}, 0));
+    }
+
+    @Test
+    public void test_6() {
+        assertEquals(1, searchInsert(new int[]{1,3}, 2));
+    }
+    @Test
+    public void test_7() {
+        assertEquals(1, searchInsert(new int[]{1,2,3,4,5,10}, 2));
+    }
+
+}
