c0a715794be35eaea977916e68330690c2faba1f
AlexanderBrovko <denorian@yandex.ru>
2022-02-13 18:43:36 +0300

    SameTree

---
+++ src/com/brovko/leetcode/easy/SameTree.java
@@ -0,0 +1,40 @@
+package com.brovko.leetcode.easy;
+
+import com.brovko.leetcode.common.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SameTree {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    @Test
+    public void test_1() {
+        TreeNode treeA = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode treeB = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+
+        assertTrue(isSameTree(treeA, treeB));
+    }
+
+    @Test
+    public void test_2() {
+        TreeNode treeA = new TreeNode(1, new TreeNode(2), null);
+        TreeNode treeB = new TreeNode(1, null, new TreeNode(3));
+
+        assertFalse(isSameTree(treeA, treeB));
+    }
+
+    @Test
+    public void test_3() {
+        TreeNode treeA = new TreeNode(1, new TreeNode(2), new TreeNode(1));
+        TreeNode treeB = new TreeNode(1, new TreeNode(1), new TreeNode(2));
+
+        assertFalse(isSameTree(treeA, treeB));
+    }
+}
