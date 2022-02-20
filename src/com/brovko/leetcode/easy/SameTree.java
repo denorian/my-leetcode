0d62a72d3d0677cac26f35e24d78ffe63ec2bbee
AlexanderBrovko <denorian@yandex.ru>
2022-02-20 10:43:39 +0300

    SymmetricTree

--- src/com/brovko/leetcode/easy/SameTree.java
+++ src/com/brovko/leetcode/easy/SameTree.java
@@ -6,6 +6,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SameTree {
+    /**
+     * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+     * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+     */
     public static boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
         if (p == null || q == null) return false;
