60b72ed3622363a9cd1047762f41564f8a2a446b
AlexanderBrovko <denorian@yandex.ru>
2022-03-29 09:55:27 +0300

    ConcatenationOfArray

---
+++ src/com/brovko/leetcode/easy/ConcatenationOfArray.java
@@ -0,0 +1,36 @@
+package com.brovko.leetcode.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class ConcatenationOfArray {
+
+    public static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+        }
+
+        return ans;
+    }
+
+    @Test
+    public void test_1() {
+        assertArrayEquals(
+            new int[]{1,2,1,1,2,1},
+            getConcatenation( new int[]{1,2,1})
+        );
+    }
+
+    @Test
+    public void test_2() {
+        int[] source = {1,3,2,1};
+        assertArrayEquals(
+            new int[]{1,3,2,1,1,3,2,1},
+            getConcatenation( new int[]{1,3,2,1})
+        );
+    }
+}
