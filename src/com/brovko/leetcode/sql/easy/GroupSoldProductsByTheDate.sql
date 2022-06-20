2c18268b9bf613e0c02f0a96476f650814c29728
AlexanderBrovko <denorian@yandex.ru>
2022-06-20 07:15:08 +0300

    1484. Group Sold Products By The Date

---
+++ src/com/brovko/leetcode/sql/easy/GroupSoldProductsByTheDate.sql
@@ -0,0 +1,6 @@
+select
+    sell_date,
+    count(distinct product) as num_sold,
+    GROUP_CONCAT(distinct product order by product ASC SEPARATOR ',') as products
+from Activities
+group by sell_date
