9571b296a898ebaa2bb4a96fca00eb0d25480569
AlexanderBrovko <denorian@yandex.ru>
2022-09-11 18:39:38 +0300

    181. Employees Earning More Than Their Managers

--- src/com/brovko/leetcode/sql/easy/EmployeesEarningMoreThanTheirManagers.sql
+++ src/com/brovko/leetcode/sql/easy/EmployeesEarningMoreThanTheirManagers.sql
@@ -2,7 +2,7 @@
 Write an SQL query to find the employees who earn more than their managers.
 Return the result table in any order.*/
 
-select e1.name as 'Employee'
-from Employee e1
-left join Employee e2 on e2.id = e1.managerId
-where e1.salary > e2.salary
+SELECT e.name as Employee
+FROM Employee e
+INNER JOIN Employee e2 ON e.managerId = e2.id
+WHERE e.salary > e2.salary
